b1d7c2696ece1a4901dbb6cf41e358ec7deda2e0
Nitish Kumar <iamnitishsah12@gmail.com>
2025-06-16 06:10:27 +0530

    Update urls.py

--- NITR-Mart-Backend/nitrmart/urls.py
+++ NITR-Mart-Backend/nitrmart/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('us/', admin.site.urls),
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
 ]
