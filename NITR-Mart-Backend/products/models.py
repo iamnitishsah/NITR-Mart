52e1fa471f31fcf64f4fb6eee5e37d6b12557295
Sanchita-nitr <sanchitapriyadarshinee@gmail.com>
2025-06-16 14:16:36 +0530

    changes

--- NITR-Mart-Backend/products/models.py
+++ NITR-Mart-Backend/products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from cloudinary.models import CloudinaryField
+from cloudinary.models import CloudinaryField # type: ignore
 
 CATEGORIES = [
     ('Electronics', 'Electronics'),
